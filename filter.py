dd3f83bdda4317220e5596b7a2d32379cba6f236
@val314159 <jmward@gmail.com>
2024-02-07 10:28:46 -0600

    first cut

---
+++ filter.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+import sys
+def out(s=None):
+    if s is None: return print()
+    return print(s, end='')
+for line in sys.stdin:
+    if 'stylesheet' in line:
+        out("<style>")
+        out(open('public/style.css').read())
+        out("</style>")
+        out()
+    else:
+        out(line)
+
